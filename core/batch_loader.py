fc622185d516689057d8056b9281565f7df0db22
Jeremy Echols <jechols@uoregon.edu>
2014-12-17 12:24:32 -0800

    Fix spacing in batch_loader.py

--- core/batch_loader.py
+++ core/batch_loader.py
@@ -257,12 +257,12 @@ class BatchLoader(object):
         try:
             title = Title.objects.get(lccn=lccn)
         except Exception, e:
-		try:
-            		management.call_command('load_titles',settings.BIB_STORAGE + '%s.xml' % lccn)
-      	    		title = Title.objects.get(lccn=lccn)
-		except Exception, e:
-			management.call_command('load_titles', 'http://chroniclingamerica.loc.gov/lccn/%s/marc.xml' % lccn)
-			title = Title.objects.get(lccn=lccn)
+            try:
+                management.call_command('load_titles', settings.BIB_STORAGE + '%s.xml' % lccn)
+                title = Title.objects.get(lccn=lccn)
+            except Exception, e:
+                management.call_command('load_titles', 'http://chroniclingamerica.loc.gov/lccn/%s/marc.xml' % lccn)
+                title = Title.objects.get(lccn=lccn)
         issue.title = title
 
         issue.batch = self.current_batch
